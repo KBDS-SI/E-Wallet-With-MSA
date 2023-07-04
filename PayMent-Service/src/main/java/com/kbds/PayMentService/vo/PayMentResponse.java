14713b1fec11bc021ed9dd2509f2c0aad32fa4d7
guijaepa <guijaepa@gmail.com>
2023-07-04 11:09:43 +0900

    TEST1111

---
+++ PayMent-Service/src/main/java/com/kbds/PayMentService/vo/PayMentResponse.java
@@ -0,0 +1,17 @@
+package com.kbds.PayMentService.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PayMentResponse {
+    private String useId;
+    private String sendId;
+    private String receiveId;
+    private BigDecimal sendAmt;
+    private LocalDateTime sendAt;
+}
