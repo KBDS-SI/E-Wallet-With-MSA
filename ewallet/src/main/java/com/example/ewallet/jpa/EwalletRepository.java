8336e851106d141561dfc0cc12ae1f3dd247399a
laugh4mile <laugh4mile@naver.com>
2023-07-07 17:02:24 +0900

    test

--- ewallet/src/main/java/com/example/ewallet/jpa/EwalletRepository.java
+++ ewallet/src/main/java/com/example/ewallet/jpa/EwalletRepository.java
@@ -1,6 +1,12 @@
 package com.example.ewallet.jpa;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface EwalletRepository extends CrudRepository<EwalletEntity, EwalletID> {
     EwalletEntity findByEwalletId(EwalletID ewalletID);
--- ewallet/src/main/java/com/example/ewallet/service/EwalletServiceImpl.java
+++ ewallet/src/main/java/com/example/ewallet/service/EwalletServiceImpl.java
@@ -2,14 +2,13 @@ package com.example.ewallet.service;
 
 import com.example.ewallet.dto.EwalletDto;
 import com.example.ewallet.jpa.EwalletEntity;
+import com.example.ewallet.jpa.EwalletID;
 import com.example.ewallet.jpa.EwalletRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaUpdate;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -25,6 +24,8 @@ public class EwalletServiceImpl implements EwalletService {
     @Autowired
     EwalletRepository ewalletRepository;
 
+    @Autowired
+    private EntityManager em;
 
     @Override
     public EwalletDto createEwallet(EwalletDto ewalletDto) {
@@ -51,63 +52,20 @@ public class EwalletServiceImpl implements EwalletService {
     }
 
     @Override
+    @Transactional
     public EwalletDto updateBalance(EwalletDto ewalletDto) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ewallet");
-        EntityManager em = emf.createEntityManager();
+        EwalletEntity ewalletEntity = ewalletRepository.findByUserId(ewalletDto.getUserId());
+        log.info("ewalletEntity : " + ewalletEntity.toString());
+        BigDecimal newAmt = new BigDecimal("0");
+        newAmt.add(ewalletEntity.getAmt());
+        log.info("newAmt : " + newAmt.toString());
 
-        em.getTransaction().begin();
-        EwalletDto ewalletDto1 = new EwalletDto();
-        try {
-            ewalletDto1 = em.find(EwalletDto.class, ewalletDto);
-            log.info("ewalletDto1 : " + ewalletDto1.toString());
+        EwalletEntity ewalletEntity1 = em.find(EwalletEntity.class, ewalletDto.getEwalletId());
+        log.info("ewalletEntity1 : " + ewalletEntity1.toString());
 
-            CriteriaBuilder cb = em.getCriteriaBuilder();
+        ewalletEntity1.setAmt(ewalletEntity1.getAmt().add(ewalletDto.getAmt()));
+        log.info("ewalletEntity1,2 : " + ewalletEntity1.toString());
 
-            CriteriaUpdate<EwalletDto> updateCriteria = cb.createCriteriaUpdate(EwalletDto.class);
-            Root<EwalletDto> root = updateCriteria.from(EwalletDto.class);
-
-            updateCriteria.set(root.get("amt"), ewalletDto1.getAmt().add(ewalletDto.getAmt()));
-            Predicate condition = cb.and(
-                    cb.equal(root.get("userId"), ewalletDto.getUserId()),
-                    cb.equal(root.get("ewalletId"), ewalletDto.getEwalletId())
-            );
-            updateCriteria.where(condition);
-
-            // 업데이트 실행
-            int updatedCount = em.createQuery(updateCriteria).executeUpdate();
-
-            // 업데이트된 엔티티 수 출력
-            log.info("Updated count: " + updatedCount);
-
-
-            // 엔티티 수정
-//            ewalletDto1.setAmt(ewalletDto1.getAmt().add(ewalletDto.getAmt()));
-
-            // merge()를 사용하여 업데이트 수행
-//            EwalletDto updateEwallet = em.merge(ewalletDto1);
-
-            // 업데이트된 엔티티 확인
-
-            // 트랜잭션 커밋
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-        } finally {
-            em.close();
-            emf.close();
-        }
-//        EwalletEntity ewalletEntity = ewalletRepository.findByUserId(ewalletDto.getUserId());
-//
-//        ModelMapper mapper = new ModelMapper();
-//        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-//
-//        EwalletDto ewalletDto1 = mapper.map(ewalletEntity, EwalletDto.class);
-//        BigDecimal finalBal = ewalletDto1.getAmt().add(ewalletDto.getAmt());
-//        ewalletDto1.setAmt(ewalletDto1.getAmt().add(ewalletDto.getAmt()));
-//        EwalletEntity ewalletEntity1 = mapper.map(ewalletDto1, EwalletEntity.class);
-//        ewalletRepository.save(ewalletEntity1);
-
-//        ewalletDto = mapper.map(ewalletEntity1, EwalletDto.class);
-        return ewalletDto1;
+        return ewalletDto;
     }
 }
