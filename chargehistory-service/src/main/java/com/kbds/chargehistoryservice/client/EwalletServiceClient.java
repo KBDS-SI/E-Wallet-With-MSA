f2dc96e68bfb24dcb6417dd08f00b99bf3d98e18
guijaepa <guijaepa@gmail.com>
2023-07-07 13:51:14 +0900

    전자지갑 금액 UPDATE 처리

--- chargehistory-service/src/main/java/com/kbds/chargehistoryservice/client/EwalletServiceClient.java
+++ chargehistory-service/src/main/java/com/kbds/chargehistoryservice/client/EwalletServiceClient.java
@@ -1,13 +1,19 @@
 package com.kbds.chargehistoryservice.client;
 
+import com.example.ewallet.vo.RequestEwallet;
 import com.example.ewallet.vo.ResponseEwallet;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "ewallet", url = "http://192.168.61.190:8000")
 public interface EwalletServiceClient {
     @GetMapping("/ewallet-service/searchWallet/{userId}")
     ResponseEntity<ResponseEwallet> getSearchEwallet(@PathVariable String userId);
+
+    @GetMapping("/ewallet-service/updateBalance")
+    ResponseEntity<ResponseEwallet> updateBalance(@RequestBody RequestEwallet requestEwallet);
+
 }
