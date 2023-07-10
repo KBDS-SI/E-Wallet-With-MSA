97e1637f3c94f4ed4491e1e157ba610649e8d913
guijaepa <guijaepa@gmail.com>
2023-07-10 09:41:23 +0900

    송금 내역 받기 수정 처리

--- PayMent-Service/src/main/java/com/kbds/PayMentService/client/EwalletServiceClient.java
+++ PayMent-Service/src/main/java/com/kbds/PayMentService/client/EwalletServiceClient.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "ewallet", url = "http://192.168.61.190:8000")
@@ -14,6 +15,6 @@ public interface EwalletServiceClient {
     @GetMapping("/ewallet-service/searchEwallet/{userId}")
     ResponseEntity<ResponseEwallet> getSearchEwallet(@PathVariable String userId);
 
-    @GetMapping("/ewallet-service/updateBalance")
+    @PostMapping("/ewallet-service/updateBalance")
     ResponseEntity<ResponseEwallet> updateBalance(@RequestBody RequestEwallet requestEwallet);
 }
